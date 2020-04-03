0534d3e1457f015c1e50ce1e927ee16a4288c4fc
amccaugh <amccaugh@gmail.com>
2020-04-03 16:07:26 -0600

    Finally! Huge bugfix, normalized each atom

--- snn_lasso.py
+++ snn_lasso.py
@@ -6,44 +6,61 @@ from scipy.optimize import minimize
 
 
 
+# =============================================================================
+# Target parameters and initialization
+# =============================================================================
+np.random.seed(1)
+N = 10 # Number of neurons / number of dictionary items
+M = 10 # Dimension of input target / output vector
+Phi = np.random.uniform(0,1,size = (M,N))
+s = np.random.uniform(0,2, size = M)
+
+# Normalize each dictionary atom (phi_i in Phi) to unit Euclidean norm
+for i in range(N):
+    Phi[:,i] /= np.linalg.norm(Phi[:,i])
+    
+#Phi = np.array([[0.3313,0.8148,0.4364],
+#                [.8835,0.3621,0.2182],
+#                [0.3313,0.4527,0.8729]])
+#s = np.array([0.5,1,1.5]) # target (desired output trying to approximate)
 
 
-# Define the dictionary and starting state
-# Phi = dictionary
-Phi = np.array([[0.3313,0.8148,0.4364],
-                [.8835,0.3621,0.2182],
-                [0.3313,0.4527,0.8729]])
-N = 3 # Number of neurons / number of dictionary items
-M = 3 # Dimension of input target / output vector
+
 vf = 1 # Threshold membrane potential
 vr = 0 # Reset membrane potential
 
-# s = target (desired output)
-s = np.array([0.5,1,1.5])
-# Lambda = static bias value
-lam = 0.1
+## Manually test the creation of w and b
+#w_test = np.zeros([N,N])
+#b_test = np.zeros(N)
+#for i in range(N):
+#    phi_i = Phi[:,i]
+#    b_test[i] = phi_i @ s
+#    for j in range(N):
+#        phi_j = Phi[:,j]
+#        w_test[i,j] = phi_i @ phi_j
 
-# Bias vector for the neurons
-b = Phi.T @ s
+
+lam = 0.1 # Lambda = static bias value for membrane current
+
+
+b = Phi.T @ s # Bias vector for the neurons (size N)
 # Weight matrix between the neurons
-w = Phi.T @ Phi
+w = Phi.T @ Phi # (size NxN)
 w_mask = 1 - np.identity(N)
 w *= w_mask
 
 
 
 # L holds the last X times a spike arrived to neuron i
-X = 4
+X = 10
 
 
-dt = 1e-3
-times = np.arange(0,10,dt)
-
-data = {}
-data_v = np.zeros([len(times), N])
-data_mu = np.zeros([len(times), N])
-data_spikes = np.zeros([len(times), N])
-
+#
+#data = {}
+#data_v = np.zeros([len(times), N])
+#data_mu = np.zeros([len(times), N])
+#data_spikes = np.zeros([len(times), N])
+#
 
 
 
@@ -64,7 +81,9 @@ def timestep(v, L, Lidx, t, dt):
             if is_spiking[n] == True:
                 L[n,Lidx[n]] = t
                 Lidx[n] = (Lidx[n] + 1) % X
-    return v, mu, L, Lidx, is_spiking
+#        import pdb
+#        pdb.set_trace()
+    return v, mu, L, Lidx, is_spiking, alpha
     
 @njit
 def run(total_time, dt):
@@ -73,55 +92,59 @@ def run(total_time, dt):
     v = np.zeros(N) # v is the membrane potential
     
     # L holds the last X times a spike arrived to neuron i
-    L = np.ones((N,X))*(-100)
-    Lidx = np.zeros(X, dtype = np.int_)
+    L = np.ones((N,X))*(-np.inf)
+    Lidx = np.zeros(N, dtype = np.int_)
 
     data_v = np.zeros((len(times), N))
     data_mu = np.zeros((len(times), N))
     data_spikes = np.zeros((len(times), N))
+    data_alpha = np.zeros((len(times), N))
     
     for i,t in enumerate(times):
-        v, mu, L, Lidx, is_spiking = timestep(v, L, Lidx, t, dt)
+        v, mu, L, Lidx, is_spiking, alpha = timestep(v, L, Lidx, t, dt)
         
         # Save data
         data_v[i,:] = v
         data_mu[i,:] = mu
         data_spikes[i,:] = is_spiking
+        data_alpha[i,:] = alpha
     
-    return data_v, data_mu, data_spikes
+    return data_v, data_mu, data_spikes, data_alpha
 
-total_time = 10
+total_time = 100
+dt = 1e-3
+times = np.arange(0,total_time,dt)
 
-data_v, data_mu, data_spikes = run(10, dt)
-#
-#plt.figure()
-#plt.plot(times, data_v[:,0])
-#plt.plot(times, data_v[:,1])
-#plt.plot(times, data_v[:,2])
-#
-#
-#plt.figure()
-#plt.plot(times, data_mu[:,0])
-#plt.plot(times, data_mu[:,1])
-#plt.plot(times, data_mu[:,2])
-#
-#plt.figure()
-#plt.plot(times, data_spikes[:,0]*1, '+')
-#plt.plot(times, data_spikes[:,1]*1.1, '+')
-#plt.plot(times, data_spikes[:,2]*1.2, '+')
+data_v, data_mu, data_spikes, data_alpha = run(total_time, dt)
 
-print("SNN LASSO solution =", np.sum(data_spikes,axis = 0)/total_time)
+#fig, axs = plt.subplots(4,1, sharex = True, sharey = False)
+#p = 0
+#[axs[p].plot(times, data_v[:,n],'.') for n in range(N)]
+#p += 1
+#[axs[p].plot(times, data_mu[:,n],'.') for n in range(N)]
+#p += 1
+#[axs[p].plot(times, np.cumsum(data_spikes[:,n]),'.') for n in range(N)]
+#p += 1
+#[axs[p].plot(times, data_alpha[:,n],'.') for n in range(N)]
+#p += 1
+#plt.tight_layout()
+
+solution_snn = np.sum(data_spikes,axis = 0)/total_time
+print("SNN LASSO solution = %s" % np.round(np.sum(data_spikes,axis = 0)/total_time,2))
 
 # =============================================================================
 # Solve using LASSO objective
 # =============================================================================
-def lasso(x,lam,Phi,b):
+def classo(x,lam,Phi,s):
     # Evaluates Lasso objective
-    f = 1/2*np.linalg.norm(b-Phi@x,2)**2 + lam*np.linalg.norm(x,1)
+    f = 1/2*np.linalg.norm(s-Phi@x,2)**2 + lam*np.linalg.norm(x,1)
     return f
 
 # Solve problem by direct optimization
-f_opt = lambda x: lasso(x,lam,Phi,s)
-x0  = np.ones(N)
-res = minimize(f_opt, x0, method='nelder-mead', options={'xtol':1e-6})
-print("Direct LASSO solution =", np.round(res.x,2))
+f_opt = lambda x: classo(x,lam,Phi,s)
+x0  = solution_snn
+res = minimize(f_opt, x0, 
+               bounds = [(0,None)]*N)
+solution_direct =  res.x
+print("Direct LASSO solution =", np.round(solution_direct,2))
+

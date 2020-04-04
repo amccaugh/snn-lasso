cb8987a132d66aa3d3064fba9ea9420529c01c24
amccaugh <amccaugh@gmail.com>
2020-04-03 18:33:18 -0600

    Convergence calculation and plots

--- snn_lasso.py
+++ snn_lasso.py
@@ -88,6 +88,8 @@ times = np.arange(0,total_time,dt)
 
 data_v, data_mu, data_spikes, data_alpha = run(total_time, dt)
 
+data_spike_rates = (np.cumsum(data_spikes,0).T/times).T
+
 #fig, axs = plt.subplots(4,1, sharex = True, sharey = False)
 #p = 0
 #[axs[p].plot(times, data_v[:,n],'.') for n in range(N)]
@@ -100,7 +102,7 @@ data_v, data_mu, data_spikes, data_alpha = run(total_time, dt)
 #p += 1
 #plt.tight_layout()
 
-solution_snn = np.sum(data_spikes,axis = 0)/total_time
+solution_snn = data_spike_rates[-1,:]
 print("SNN LASSO solution = %s" % np.round(np.sum(data_spikes,axis = 0)/total_time,2))
 
 # =============================================================================
@@ -119,3 +121,26 @@ res = minimize(f_opt, x0,
 solution_direct =  res.x
 print("Direct LASSO solution =", np.round(solution_direct,2))
 
+
+
+# Calculate convergence
+def convergence_vs_time(times, data_spike_rates, solution_direct):
+    num_points = 1000
+    downsample = len(times)//num_points
+    E_star = classo(solution_direct, lam, Phi, s)
+    convergence = []
+    times_downsampled = []
+    for n in range(downsample,len(times),downsample):
+        E_t = classo(data_spike_rates[n,:],lam,Phi,s)
+        convergence.append((E_t-E_star)/E_star)
+        times_downsampled.append(times[n])
+    return times_downsampled, convergence
+
+
+t, c = convergence_vs_time(times, data_spike_rates, solution_direct)
+fig, ax = plt.subplots()
+ax.plot(t,c,'.')
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlabel('Time')
+ax.set_ylabel('Convergence (E(t)-E*)/E*')
